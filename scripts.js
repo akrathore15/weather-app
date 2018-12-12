055e29755697df9187c059adf45676f1ebf270bb
pattonkb <pattonkb0011@gmail.com>
2018-12-12 12:30:31 -0500

    hard coded today/tomorrow html sections and added more JS

--- scripts.js
+++ scripts.js
@@ -15,22 +15,48 @@
         return open.apply(this, args);
     };
 })();
-const weatherToday = document.querySelector(".weather-today");
+
+// const weatherToday = document.querySelector(".weather-today");
+
+function getTomorrowUnixTime() {
+    var today = new Date();
+    var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+    var ts = Math.round(tomorrow/1000);
+    // console.log(tomorrow);
+    // console.log(ts);
+    return ts;
+}
+
 //api call to dark sky's database
 const weatherRequest = new XMLHttpRequest();
 weatherRequest.onreadystatechange = function () {
     if (weatherRequest.readyState === 4 && weatherRequest.status === 200) {
         const weatherResponse = JSON.parse(weatherRequest.responseText);
         const weatherData = weatherResponse;
-        const weatherDataCurrently = weatherData.currently;
-        console.log(weatherDataCurrently);
-        const currentWeather = document.createElement("p");
-        currentWeather.innerHTML = `
-            Current Tempurature: ${weatherDataCurrently.temperature}&deg; F <br>
-            Summary: ${weatherDataCurrently.summary}
-        `;
-        weatherToday.appendChild(currentWeather);
-}
+        const weatherDataCurrent = weatherData.currently;
+        const currentTemp = document.querySelector(".current-temp").innerHTML = weatherDataCurrent.temperature;
+        const precipProb = document.querySelector(".precip-prob").innerHTML = weatherDataCurrent.precipProbability;
+        const wind = document.querySelector(".wind").innerHTML = weatherDataCurrent.windSpeed;
+        const uvIndex = document.querySelector(".uv-index").innerHTML = weatherDataCurrent.uvIndex;
+        const summary = document.querySelector(".summary").innerHTML = weatherDataCurrent.summary;
+    }
 };
-weatherRequest.open("GET", "https://api.darksky.net/forecast/db27ab4384ceebe7e5e55d9208d5d871/38.033747,-78.468363");
+weatherRequest.open("GET", "https://api.darksky.net/forecast/db27ab4384ceebe7e5e55d9208d5d871/38.033747,-78.468363/");
 weatherRequest.send();
+
+const weatherTomorrowRequest = new XMLHttpRequest();
+weatherTomorrowRequest.onreadystatechange = function () {
+    if (weatherTomorrowRequest.readyState === 4 && weatherTomorrowRequest.status === 200) {
+        const weatherResponse = JSON.parse(weatherTomorrowRequest.responseText);
+        const weatherData = weatherResponse;
+        const weatherDataTomorrow = weatherData.daily.data[0];
+        console.log(weatherDataTomorrow);
+        const tomorrowTemp = document.querySelector(".tomorrow-temp").innerHTML = weatherDataTomorrow.temperatureHigh;
+        const tomorrowprecipProb = document.querySelector(".tomorrow-precip-prob").innerHTML = weatherDataTomorrow.precipProbability;
+        const tomorrowWind = document.querySelector(".tomorrow-wind").innerHTML = weatherDataTomorrow.windSpeed;
+        const tomorrowUvIndex = document.querySelector(".tomorrow-uv-index").innerHTML = weatherDataTomorrow.uvIndex;
+        const tomorrowSummary = document.querySelector(".tomorrow-summary").innerHTML = weatherDataTomorrow.summary;
+    }
+};
+weatherTomorrowRequest.open("GET", `https://api.darksky.net/forecast/db27ab4384ceebe7e5e55d9208d5d871/42.3601,-71.0589,${getTomorrowUnixTime()}?exclude=currently,flags`);
+weatherTomorrowRequest.send();
